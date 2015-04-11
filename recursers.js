5fbfea8de70ddc686dafdd24b690893f98eb9475
Chris Ball <chris@printf.net>
2015-04-11 22:30:12 +0000

    Remove some unused code

--- recursers.js
+++ recursers.js
@@ -31,19 +31,9 @@ if (Meteor.isClient) {
       // Prevent default form submit
       return false;
     },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    }
   });
 
   Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
-    },
     "click .toggle-private": function () {
       Meteor.call("setPrivate", this._id, ! this.private);
     }
@@ -85,25 +75,6 @@ Meteor.methods({
       username: Meteor.user().profile.name,
       image: Meteor.user().services.recursecenter.image 
     });
-  },
-
-  deleteTask: function (taskId) {
-    var task = LastUpdates.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can delete it
-      throw new Meteor.Error("not-authorized");
-    }
-
-    LastUpdates.remove(taskId);
-  },
-  setChecked: function (taskId, setChecked) {
-    var task = LastUpdates.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can check it off
-      throw new Meteor.Error("not-authorized");
-    }
-
-    LastUpdates.update(taskId, { $set: { checked: setChecked} });
   },
   setPrivate: function (taskId, setToPrivate) {
     var task = LastUpdates.findOne(taskId);
